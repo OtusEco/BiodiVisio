2d945c8dbaa549fa5f366f2cf2cfa45a70737978
Adri Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2026-04-27 21:09:11 +0200

    [Pubspec] v1.1.1

--- lib/map/widgets/view.dart
+++ lib/map/widgets/view.dart
@@ -46,7 +46,7 @@ class MapView extends StatelessWidget {
       children: [
         TileLayer(
           urlTemplate: baseMaps[currentBaseMap]!,
-          userAgentPackageName: 'fr.otuseco.biodivisio/1.1.0',
+          userAgentPackageName: 'fr.otuseco.biodivisio/1.1.1',
         ),
 
         MarkerLayer(markers: userLocationMarker),
